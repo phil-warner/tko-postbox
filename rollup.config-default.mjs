473d2e85c1d787c35c45d4ad18cad797f1f034c7
phil-warner <philip.warner@gmail.com>
2025-01-23 21:39:47 +0000

    ko now resolves to tko

--- rollup.config-default.mjs
+++ rollup.config-default.mjs
@@ -4,7 +4,6 @@ import html from "rollup-plugin-html";
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import alias from '@rollup/plugin-alias';
 
 export default [{
   input: ['src/public/js/main.js'],
@@ -13,7 +12,26 @@ export default [{
   },
   plugins: [
     nodeResolve({ browser: true, preferBuiltins: false }),
-    commonjs(),
+    {
+      name: 'knockout-fixed',
+      resolveId(source) {
+        if (source === 'knockout') {
+          return 'knockout-fixed';
+        }
+        return null;
+      },
+      load(id) {
+        if (id === 'knockout-fixed') {
+          return `
+            import ko from '@tko/build.reference';
+            export default ko;
+          `;
+        }
+      }
+    },
+    commonjs({
+      requireReturnsDefault: 'auto'
+    }),
     html({
       include: "**/*.html"
     }),
@@ -21,11 +39,6 @@ export default [{
       entryFileName: 'default.bundle.js'
     }),
     json(),
-    alias({
-      entries: [
-        { find: 'knockout', replacement: '@tko/build.reference' }
-      ]
-    }),
     copy({
       targets: [
         { src: ['src/app.js', 'src/config', 'src/models', 'src/routers', 'src/views', 'src/public'], dest: 'dist' }
--- src/routers/app.js
+++ src/routers/app.js
@@ -2,9 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.set('Cache-control', 'no-store');
-    res.set('Pragma', 'no-cache');
-    res.render('pages/index', { time: Date.now() });
+  res.set('Cache-control', 'no-store');
+  res.set('Pragma', 'no-cache');
+  res.render('pages/index', { time: Date.now() });
 });
 
 export default router;
